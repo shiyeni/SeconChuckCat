3afa090e2f13f3816d0d65f963f9983d63c2001a
shiyeni <ongeziwe@quirky30.co.za>
2019-12-13 12:36:37 +0200

    Categories

---
+++ ChckNorrisJokesLibrary/jokeGenerator.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChckNorrisJokesLibrary
+{
+   public class JokeGenerator
+    {
+        public async Task<string> GetRandomJoke()
+        {
+          var client = new HttpClient();
+         string randomJoke = await client.GetStringAsync("https://api.datamuse.com/words?sp=girl&md=d&max=1");
+
+        var joke = JsonConvert.DeserializeObject<Joke>(randomJoke);
+
+         return joke.value;
+        }
+
+
+        public async Task<string[]> GetCategories()
+        {
+            var client = new HttpClient();
+            string randomCategories = await client.GetStringAsync("https://api.datamuse.com/words?sp=girl&md=d&max=1");
+
+            var categories = JsonConvert.DeserializeObject<string[]>(randomCategories);
+
+            return categories;
+        }
+
+        public async Task<string> GetRandomCatJ(string category)
+        {
+            var client = new HttpClient();
+            string randomCatJ = await client.GetStringAsync("https://api.datamuse.com/words?sp=girl&md=d&max=1" + category);
+            var joke = JsonConvert.DeserializeObject<Joke>(randomCatJ);
+            return joke.value;
+        }
+    }
+}
